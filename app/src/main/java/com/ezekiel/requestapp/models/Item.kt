ac290fe601fd65c4d56d8a3e7fb963f473f048eb
Ezekiel12345 <you@example.com>
2021-07-20 13:10:11 +0100

    Api call app

---
+++ app/src/main/java/com/ezekiel/requestapp/models/Item.kt
@@ -0,0 +1,7 @@
+package com.ezekiel.requestapp.models
+
+data class Item(
+    val id: Int,
+    val name: String,
+    val price: Int
+)
---
+++ app/src/main/java/com/ezekiel/requestapp/models/Student.kt
@@ -0,0 +1,10 @@
+package com.ezekiel.requestapp.models
+
+import com.google.gson.annotations.SerializedName
+
+data class Student(
+    var name: String,
+    var seat: String,
+    @SerializedName("class")
+    var classNames: String
+)
